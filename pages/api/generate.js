de5fceae138630e0138a5bd9bd721a23d4e1ac03
OrbitMiami <xbotmate007@gmail.com>
2025-05-24 17:54:04 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,14 +4,12 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
-      "Authorization": `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Authorization": `Token ${process.env.NEW_REPLICATE_API_TOKEN}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      version: "db21e45a3e738d0db3d265d204127f3f42085b4b26c9c640fe6e31f8b4c6f89c", // SD 1.5 default
-      input: {
-        prompt: prompt,
-      },
+      version: "db21e45a3e738d0db3d265d204127f3f42085b4b26c9c640fe6e31f8b4c6f89c",
+      input: { prompt },
     }),
   });
 
